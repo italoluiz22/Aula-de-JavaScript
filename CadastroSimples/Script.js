af34cc8c552a08df6bb3549b66b55e94107de84a
italoluiz22 <italoirias29@gmail.com>
2026-02-22 15:41:19 -0300

    Atividade de cadastro Simples

---
+++ CadastroSimples/Script.js
@@ -0,0 +1,8 @@
+let nome=prompt("Digite seu nome")
+let idade=prompt("Digite seu idade")
+let cidade = prompt("Digite sua cidade:");
+let mensagem = "Olá " + nome + ", você tem " + idade + " anos e mora em " + cidade + ".";
+
+console.log(mensagem)
+alert(mensagem)
+
